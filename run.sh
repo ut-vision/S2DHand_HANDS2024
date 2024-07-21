d9dc04a98191ed017204bd9c9445082e581ce4b0
Ruicong Liu <lruicong@iis.u-tokyo.ac.jp>
2024-07-21 22:41:56 +0800

    update run.sh

--- run.sh
+++ run.sh
@@ -12,12 +12,16 @@ do
 done
 
 #Adaptation under in-dataset scenarios
-for setup in 0 #1
+for setup in 0 1
 do
-  for pair in 0,1 #0,2 0,3 1,2 1,3 2,3
+  for pair in 0,1 0,2 0,3 1,2 1,3 2,3
   do
+#    On the val set
     python3 adapt_detnet_dual.py -trs ah -tes ah --set val --root_idx 0 --pic 1024 --resume -eid 34 --epochs 10 --start_epoch 1 --gpus ${gpu} --checkpoint in_dataset_adapt --setup ${setup} --pair ${pair}
 #    python3 adapt_detnet_dual.py -trs ah -tes ah --evaluate -eid 10 --gpus 0 --pic -1 --checkpoint in_dataset_adapt --setup ${setup} --pair ${pair}
+
+#    On the test set, run this for final submission
+#    python3 adapt_detnet_dual.py -trs ah -tes ah --set test --root_idx 0 --pic 1024 --resume -eid 34 --epochs 10 --start_epoch 1 --gpus ${gpu} --checkpoint in_dataset_adapt --setup ${setup} --pair ${pair}
   done
 done
-#python3 gen_submit_json.py --evaluation_dir in_dataset_adapt/evaluation/ah -eid 10 --savepath assemblyhands_test_joint_3d_eccv24_pred.json
+python3 gen_submit_json.py --evaluation_dir in_dataset_adapt/evaluation/ah -eid 10 --savepath assemblyhands_test_joint_3d_eccv24_pred.json
